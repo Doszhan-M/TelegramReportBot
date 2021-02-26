4b96a46de74700f8aa1a33448fd21d5642c2f701
Doszhan <doszhan@mail.com>
2021-02-26 08:37:27 +0600

    update admin panel

--- TelegramServer/server/admin.py
+++ TelegramServer/server/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from .models import Payment, Score, Lender, Borrower
 
 
-# class PaymentAdmin(admin.ModelAdmin):
-#     list_display = ('payment_date', 'payment_status', 'monthly_payment',)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payment_date', 'monthly_payment',)
 
 
-# class ScoreAdmin(admin.ModelAdmin):
-#     list_display = ('total', 'total_payment',)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('date_score', 'payment_status', 'monthly_payment',  'total_payment', 'leftover',  'total',)
 
 
-admin.site.register(Payment)
-admin.site.register(Score)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Score, ScoreAdmin)
 admin.site.register(Lender)
 admin.site.register(Borrower)
 
